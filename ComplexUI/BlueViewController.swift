7586c727edec21d89d84d6ef4bc64d6acda2137f
Shantaram K <shantaramk@Mipladmin-6.local>
2019-04-17 17:09:37 +0530

    first commit

---
+++ ComplexUI/BlueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BlueViewController.swift
+//  ComplexUI
+//
+//  Created by Shantaram K on 17/04/19.
+//  Copyright © 2019 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
